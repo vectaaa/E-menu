a4ace522e099ed43cd47b5ec7a9c22c6ef1a0f59
vectaaa <victoronuchukwu16@gmail.com>
2022-11-26 22:26:06 +0100

    Restuarant Account Creation

--- login.js
+++ login.js
@@ -1,4 +1,85 @@
 $(document).ready(function (){
+    const form = document.getElementById('nextBtnLg');
+    const ownersEmail = document.getElementById('lgemail');
+    const ownersPassword = document.getElementById('lgpassword');
+
+    
+    
+    
+    
+    // form.addEventListener('submit', e => {
+    //     // e.preventDefault();
+    
+    //     validateInputs();
+    // });
+    
+    // const onFormSubmit = (e)=> {
+    //     e.preventDefault();
+    //     alert(ownersName.value)
+    
+    // }
+    
+    // form.addEventListener('submit', onFormSubmit);
+    
+    
+    //Here we set the error method
+    const setError = (element, message) => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+    
+        errorDisplay.innerText = message;
+        inputControl.classList.add('error');
+        inputControl.classList.remove('success');
+    
+    }
+    
+    //Here we set the success method
+    const setSuccess = element => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+    
+        errorDisplay.innerText = '';
+        inputControl.classList.add('success');
+        inputControl.classList.remove('error');
+    }
+    
+    //This is for the email validation 
+    const isValidEmail = ownersEmail => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(ownersEmail).toLowerCase());
+        
+    }
+    
+    const validateInputs = () => {
+        const ownersEmailValue = ownersEmail.value.trim();
+        const ownersPasswordValue = ownersPassword.value.trim();
+        
+        if(ownersEmailValue === '') {
+            setError(ownersEmail, 'Owners Name is required')
+        } else if(!isValidEmail(ownersEmailValue)){
+            setError(ownersEmail, 'Provide a vaild email address');
+        }
+        else{
+             setSuccess(ownersEmail);
+        }
+    
+        if(ownersPassword === '') {
+            setError(ownersPassword, 'Password is required');
+        } else if (ownersPasswordValue.length < 8){
+            setError(ownersPassword, 'Password must be atleast 8 characters')
+        } else {
+            setSuccess(ownersPassword);
+        }
+    }
+    
+    $('.nextBtnLg').click(function (e) { 
+        e.preventDefault();
+        validateInputs();
+    
+        console.log("validated")
+    });
+
+
     let obj = {
 
     }
--- registration.js
+++ registration.js
@@ -1,10 +1,132 @@
 $(document).ready(function () {
+   
+const form = document.getElementById('nextBtn');
+const ownersName = document.getElementById('ownersName');
+const ownersEmail = document.getElementById('email');
+const ownersPassword = document.getElementById('password');
+const ownersVPassword = document.getElementById('cpassword');
+const ownersResName = document.getElementById('restaurantName');
+const ownersResAddress = document.getElementById('restaurantAddress');
+const ownersPhoneNumber = document.getElementById('phoneNumber');
+
+
+
+
+// form.addEventListener('submit', e => {
+//     // e.preventDefault();
+
+//     validateInputs();
+// });
+
+// const onFormSubmit = (e)=> {
+//     e.preventDefault();
+//     alert(ownersName.value)
+
+// }
+
+// form.addEventListener('submit', onFormSubmit);
+
+
+//Here we set the error method
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+    return false;
+
+}
+
+//Here we set the success method
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+    return true;
+}
+
+//This is for the email validation 
+const isValidEmail = ownersEmail => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(ownersEmail).toLowerCase());
     
+}
+
+const validateInputs = () => {
+    const ownersNameValue = ownersName.value.trim();
+    const ownersEmailValue = ownersEmail.value.trim();
+    const ownersPasswordValue = ownersPassword.value.trim();
+    const ownersVPasswordValue = ownersVPassword.value.trim();
+    const ownersResNameValue = ownersResName.value.trim();
+    const ownersResAddressValue = ownersResAddress.value.trim();
+    const ownersPhoneNumberValue = ownersPhoneNumber.value.trim();
+    
+    let result = false;
+
+
+    if(ownersNameValue === '') {
+        result = setError(ownersName, 'Owners Name is required')
+    } else{
+         result = setSuccess(ownersName);
+    }
+
+    if(ownersEmailValue === '') {
+        result = setError(ownersEmail, 'Owners Name is required')
+    } else if(!isValidEmail(ownersEmailValue)){
+        result = setError(ownersEmail, 'Provide a vaild email address');
+    }
+    else{
+        result = setSuccess(ownersEmail);
+    }
+
+    if(ownersPasswordValue === '') {
+        result = setError(ownersPassword, 'Password is required');
+    } else if (ownersPasswordValue.length < 8){
+        result = setError(ownersPassword, 'Password must be atleast 8 characters')
+    } else {
+        result =  setSuccess(ownersPassword);
+    }
+
+    if(ownersVPasswordValue === '') {
+        result = setError(ownersVPassword, 'Password is required');
+    } else if (ownersVPasswordValue !== ownersPasswordValue){
+        result = setError(ownersVPassword, 'Passwords must be identical')
+    } else {
+        result = setSuccess(ownersVPassword);
+    }   
+
+    if(ownersResNameValue === '') {
+        result = setError(ownersResName, 'Restuarant Name is required')
+    } else{
+        result = setSuccess(ownersResName);
+    }
+
+    if(ownersResAddressValue === '') {
+        result = setError(ownersResAddress, 'Restuarant Address is required')
+    } else{
+        result =  setSuccess(ownersResAddress);
+    }
+
+    if(ownersPhoneNumberValue === '') {
+        result = setError(ownersPhoneNumber, 'PhoneNumber is required')
+    } else{
+        result = setSuccess(ownersPhoneNumber);
+    }
+
+    return result;
+}
+
+
+
    
     let obj = {
 
     }
-
 
     $('#cacFileName').change(()=>{
         imgurlConvert().then(result=>{
@@ -24,15 +146,25 @@ $(document).ready(function () {
         })
     }
 
+
     // $('').attr('src', value);
 
     $('.nextBtn').click(function (e) { 
         e.preventDefault();
 
-        obj.name = $('#ownersName').val();
+        let res = validateInputs();
+
+        if(res == false){
+            console.log('wrong validation')
+            return
+        }
+        console.log("validated")
+
+        obj.ownersName = $('#ownersName').val();
         obj.email = $('#email').val();
         obj.password = $('#password').val();
-        obj.restaurant = $('#restaurantName').val();
+        // obj.cpassword = $('#cpassword').val();
+        obj.restaurantName = $('#restaurantName').val();
         obj.restaurantAddress = $('#restaurantAddress').val();
         obj.phoneNumber = $('#phoneNumber').val();
 
@@ -67,7 +199,6 @@ $(document).ready(function () {
                 window.location = "registration.html"
             },
             // error:(error)=>{
-
             //     alert(error)
             // }
         
